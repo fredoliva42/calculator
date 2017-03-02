6894883f8a141af707fb2e95435d813b5ccdfa44
Frederic Oliva <fredoliva42@gmail.com>
2017-03-02 00:56:54 -0800

    created new fonction calcul BMI

--- js/scripts.js
+++ js/scripts.js
@@ -17,4 +17,14 @@ var divide = function(number1, number2) {
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1, number2));
+alert(divide(number1, number2));
+
+
+var Weight = parseInt(prompt("Enter your Weight in Pounds:"));
+var Height = parseFloat(prompt("Enter your Height in Inches: (1 foot = 12 inches)"));
+
+var bmi = function (Weight, Height) {
+  return (Weight / (Height * Height)) * 703;
+};
+
+alert(bmi(Weight, Height));
